a09512fcf99e839a227c503ede78ea1dcd379100
syedshubairraza <shubair@chargeup.club>
2024-05-26 14:11:44 +0500

    Added products page and template of purchase page.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  // Outlet,
+  Outlet,
   createRoutesFromElements,
   Route,
-  // ScrollRestoration,
+  ScrollRestoration,
 } from "react-router-dom";
-// import Footer from "./components/home/Footer/Footer";
-// import FooterBottom from "./components/home/Footer/FooterBottom";
-// import Header from "./components/home/Header/Header";
-// import HeaderBottom from "./components/home/Header/HeaderBottom";
-// import SpecialCase from "./components/SpecialCase/SpecialCase";
+import Footer from "./components/home/Footer/Footer";
+import FooterBottom from "./components/home/Footer/FooterBottom";
+import Header from "./components/home/Header/Header";
+import HeaderBottom from "./components/home/Header/HeaderBottom";
+import SpecialCase from "./components/SpecialCase/SpecialCase";
 import About from "./pages/About/About";
 import SignIn from "./pages/Account/SignIn";
 import SignUp from "./pages/Account/SignUp";
@@ -24,39 +24,37 @@ import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import Shop from "./pages/Shop/Shop";
 import Admin from "./pages/Admin/Admin";
 import Products from "./pages/Products";
-// import { useSelector } from "react-redux";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
-// const Layout = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <HeaderBottom />
-//       <SpecialCase />
-//       <ScrollRestoration />
-//       <Outlet />
-//       <Footer />
-//       <FooterBottom />
-//     </div>
-//   );
-// };
+import Purchases from "./pages/Purchases";
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <HeaderBottom />
+      <SpecialCase />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+      <FooterBottom />
+    </div>
+  );
+};
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route mode="absolute" path="/admin">
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/admin" element={<ProtectedRoutes />}>
         <Route path="/admin/" element={<Admin />}></Route>
         <Route path="/admin/products" element={<Products />}></Route>
-        <Route path="/admin/purchases" element={<About />}></Route>
+        <Route path="/admin/purchases" element={<Purchases />}></Route>
       </Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/" element={<SignIn />}></Route>
-      <Route path="/" element={<ProtectedRoutes />}>
-        {/* ==================== Header Navlink Start here =================== */}
-        <Route path="/home" element={<Home />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/journal" element={<Journal />}></Route>
-        {/* ==================== Header Navlink End here ===================== */}
         <Route path="/offer" element={<Offer />}></Route>
         <Route path="/product/:_id" element={<ProductDetails />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
---
+++ src/pages/Purchases/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminHeader from "../../components/AdminLayout/AdminHeader";
+
+const Purchases = () => {
+  return (
+    <div className="w-full pb-20">
+      <AdminHeader />
+    </div>
+  );
+};
+
+export default Purchases;
