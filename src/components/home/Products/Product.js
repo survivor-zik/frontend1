d82b046ba4803e87aa03bc03f169b86ace22d0df
syedshubairraza <shubair@chargeup.club>
2024-06-09 13:23:41 +0500

    removed compare button in product details.

--- src/components/home/Products/Product.js
+++ src/components/home/Products/Product.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BsSuitHeartFill } from "react-icons/bs";
-import { GiReturnArrow } from "react-icons/gi";
 import { FaShoppingCart } from "react-icons/fa";
 import { MdOutlineLabelImportant } from "react-icons/md";
 import Image from "../../designLayouts/Image";
@@ -26,6 +25,9 @@ const Product = (props) => {
       },
     });
   };
+  const handleBuyNow = () => {
+    navigate("/paymentgateway");
+  };
   return (
     <div className="w-full relative group">
       <div className="max-w-80 max-h-80 relative overflow-y-hidden ">
@@ -35,12 +37,15 @@ const Product = (props) => {
         <div className="absolute top-6 left-8">
           {props.badge && <Badge text="New" />}
         </div>
-        <div className="w-full h-32 absolute bg-white -bottom-[130px] group-hover:bottom-0 duration-700">
+        <div className="w-full h-30 absolute bg-white -bottom-[130px] group-hover:bottom-0 duration-700">
           <ul className="w-full h-full flex flex-col items-end justify-center gap-2 font-titleFont px-2 border-l border-r">
-            <li className="text-[#767676] hover:text-primeColor text-sm font-normal border-b-[1px] border-b-gray-200 hover:border-b-primeColor flex items-center justify-end gap-2 hover:cursor-pointer pb-1 duration-300 w-full">
-              Compare
+            <li
+              className="text-[#767676] hover:text-primeColor text-sm font-normal border-b-[1px] border-b-gray-200 hover:border-b-primeColor flex items-center justify-end gap-2 hover:cursor-pointer pb-1 duration-300 w-full"
+              onClick={handleBuyNow}
+            >
+              Buy Now
               <span>
-                <GiReturnArrow />
+                <BsSuitHeartFill />
               </span>
             </li>
             <li
@@ -71,12 +76,6 @@ const Product = (props) => {
               View Details
               <span className="text-lg">
                 <MdOutlineLabelImportant />
-              </span>
-            </li>
-            <li className="text-[#767676] hover:text-primeColor text-sm font-normal border-b-[1px] border-b-gray-200 hover:border-b-primeColor flex items-center justify-end gap-2 hover:cursor-pointer pb-1 duration-300 w-full">
-              Add to Wish List
-              <span>
-                <BsSuitHeartFill />
               </span>
             </li>
           </ul>
