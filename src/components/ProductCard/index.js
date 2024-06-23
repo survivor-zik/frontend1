1aa6bed87fb203de62eb75fb3fe957410df732d2
syedshubairraza <shubair@chargeup.club>
2024-06-23 23:35:02 +0500

    again

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
   const [openModal, setOpenModal] = useState(false);
   return (
     <div className="w-full container mx-auto justify-center rounded-xl grid grid-cols-6 mb-4 border py-2 items-center bg-[#FAF9F6] hover:shadow-xl transition-all duration-300">
-      <div className="flex col-span-5 mdl:col-span-2 products-center gap-4 ml-4 items-center">
+      <div className="flex col-span-6 mdl:col-span-2 products-center gap-4 ml-4 items-center">
         <img
           className="w-32 h-32"
           src={`https://mathematical-lavinia-survivor.koyeb.app/products/image/${product.iden}`}
@@ -17,17 +17,17 @@ const ProductCard = ({ product }) => {
           {product.name.slice(0, 25)}...
         </h1>
       </div>
-      <div className="col-span-4 md:col-span-4 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
+      <div className="col-span-6 md:col-span-4 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
         <div className="flex w-1/4 products-center text-lg font-semibold">
           PKR {product.price}
         </div>
         <div className="w-1/4 flex products-center gap-6 text-lg">
           <p>{product.categories}</p>
         </div>
-        <div className="w-1/4 flex products-center font-titleFont font-bold text-lg">
+        <div className="w-1/2 md:w-1/4 flex products-center font-titleFont font-bold text-lg">
           <p>{product.description.slice(0, 25)}...</p>
         </div>
-        <div className="w-1/4">
+        <div className="hidden md:block w-1/4">
           <div className="flex flex-col justify-start w-[50%]">
             <button
               className="px-2 py-3 mb-2 rounded-lg bg-primeColor text-white hover:bg-black"
@@ -44,6 +44,24 @@ const ProductCard = ({ product }) => {
               {deleting ? "Deleting" : "Delete"}
             </button>
           </div>
+        </div>
+      </div>
+      <div className="flex md:hidden col-span-6 mx-4">
+        <div className="flex flex-col w-full">
+          <button
+            className="px-2 py-3 mb-2 rounded-lg bg-primeColor text-white hover:bg-black"
+            onClick={() => setOpenModal(true)}
+            disabled={deleting}
+          >
+            Edit
+          </button>
+          <button
+            className="px-2 py-3 rounded-lg bg-red-500 text-white hover:bg-red-700 transition-all duration-300"
+            onClick={() => handleDelete(product.iden, setDeleting)}
+            disabled={deleting}
+          >
+            {deleting ? "Deleting" : "Delete"}
+          </button>
         </div>
       </div>
       <EditModal
--- src/components/UsersCard/index.js
+++ src/components/UsersCard/index.js
@@ -7,20 +7,20 @@ const UsersCard = ({ user }) => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div className="w-full container mx-auto justify-center rounded-xl grid grid-cols-5 mb-4 border px-4 py-2 items-center bg-[#FAF9F6] hover:shadow-xl transition-all duration-300">
-      <div className="break-all flex products-center text-lg font-semibold">
+    <div className="w-full container mx-auto justify-center flex flex-col rounded-xl md:grid md:grid-cols-5 mb-4 border px-4 py-2 items-center bg-[#FAF9F6] hover:shadow-xl transition-all duration-300">
+      <div className="w-full md:w-auto justify-center break-all flex products-center text-lg font-semibold">
         {user.full_name}
       </div>
-      <div className="break-all px-4 flex products-center text-lg font-semibold">
+      <div className="w-full md:w-auto justify-center break-all px-4 flex products-center text-lg font-semibold">
         {user.username}
       </div>
-      <div className="break-all px-4 flex products-center text-lg font-semibold">
+      <div className="w-full md:w-auto justify-center break-all px-4 flex products-center text-lg font-semibold">
         {user.email}
       </div>
-      <div className="flex products-center text-lg font-semibold">
+      <div className="w-full md:w-auto justify-center flex products-center text-lg font-semibold">
         {user.role}
       </div>
-      <div className="flex products-center text-lg font-semibold">
+      <div className="w-full md:w-auto justify-center flex products-center text-lg font-semibold">
         <div className="flex flex-col justify-start w-[50%]">
           <button
             className="px-2 py-3 mb-2 rounded-lg bg-primeColor text-white hover:bg-black"
